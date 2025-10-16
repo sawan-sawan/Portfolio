8be80eb0fc7b81dac823320cd96b107f3cc69383
sawan-sawan <evilistno@gmail.com>
2025-10-15 16:28:02 -0900

    contactpage

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { React, useState  } from 'react';
 import './Contact.css';
 import { useInView } from 'react-intersection-observer';
 
@@ -9,7 +9,11 @@ export default function Contact() {
         event.preventDefault();
         setResult("Sending....");
         const formData = new FormData(event.target);
-        formData.append("access_key", "YOUR_WEB3FORMS_ACCESS_KEY"); 
+
+        // ===================================================================
+        // == STEP 3: APNI ACCESS KEY YAHAAN PASTE KAREIN ==
+        // ===================================================================
+        formData.append("access_key", "5ebb4919-c7fe-42cc-b5c4-ec4318a7f772"); 
 
         const res = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
@@ -54,13 +58,10 @@ export default function Contact() {
                             <svg className="contact-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                             <span className="contact-text">evilistno@gmail.com</span>
                         </li>
-                        
-                        {/* ## Mobile Number Added Here ## */}
                         <li className="contact-item">
                            <svg className="contact-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
                            <span className="contact-text">+91 8837792427</span>
                         </li>
-                        
                         <li className="contact-item">
                             <svg className="contact-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                             <span className="contact-text">Phillaur, Punjab, India</span>
